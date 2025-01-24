9ca1dc8727e7e990e367ce5d7afc9aa023383e64
Worsleychase <68138593+Worsleychase@users.noreply.github.com>
2025-01-23 22:03:42 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,19 @@
 function binarySearch(list, element) {
+    if (list.length == 0) {return -1;}
+
+    var L = 0;
+    var R = list.length - 1;
+    while (R >= L) {
+        var m = Math.floor((R+L) / 2); // floor needed because m can sometimes be a float, such as 0.5, which isn't a proper index.
+        currentElement = list[m]
+        if (currentElement < element) { // Go right
+            L = m + 1;
+        } 
+        else if (currentElement > element){ // Go left
+            R = m - 1;
+        } else {
+            return m;
+        }
+    }
     return -1;
 }

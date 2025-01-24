e1637fae5cf3ecd0034f14e02c9715d19a80c7fa
Worsleychase <68138593+Worsleychase@users.noreply.github.com>
2025-01-23 22:48:19 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,15 +1,15 @@
 function binarySearch(list, element) {
     if (list.length == 0) {return -1;}
 
+    console.log(element);
     var L = 0;
     var R = list.length - 1;
     while (R >= L) {
         var m = Math.floor((R+L) / 2); // floor needed because m can sometimes be a float, such as 0.5, which isn't a proper index.
-        currentElement = list[m]
-        if (currentElement < element) { // Go right
+        if (list[m] < element) { // Go right
             L = m + 1;
         } 
-        else if (currentElement > element){ // Go left
+        else if (list[m] > element){ // Go left
             R = m - 1;
         } else {
             return m;
@@ -17,3 +17,8 @@ function binarySearch(list, element) {
     }
     return -1;
 }
+console.log("binarySearch([20,20,20],20)=  " + binarySearch([20,20,20],20));
+console.log("binarySearch([20,20,20],50)= " + binarySearch([20,20,20],50));
+console.log("binarySearch([20,20,20],19) = " + binarySearch([20,20,20],19));
+console.log("binarySearch([20,20,20],-5)= " + binarySearch([20,20,20],-5));
+console.log("binarySearch([20,20,20],0) = " + binarySearch([20,20,20],0));
